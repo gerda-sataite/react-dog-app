9d37e0494f4a14bd2008f590c569245a0a21948b
gerda-sataite <gerda.sataite@gmail.com>
2020-04-03 12:54:10 +0100

    Made toggle button work so that the message appears and disappears upon click

--- src/components/NoDogs.js
+++ src/components/NoDogs.js
@@ -4,18 +4,35 @@ import { Button } from 'grommet';
 function NoDogs() {
     const [message, setMessage] = React.useState("");
 
+    const [noShow, setNoShow] = React.useState("");
+
     const toggleNoDogs = () => {
         setMessage(!message);
     }
+
+    const handleClick = () => {
+        // two situations
+        // situation 1: blank message
+        if (message === '') {
+            console.log('situation 1!')
+            setMessage('You are horrible! Would you like to see a cat?')
+        
+        } else {
+            console.log('situation 2!')
+            setMessage('')
+        }
+    }
+
     return (
         <div>
             <p>
                 <Button
                     label="I don't like dogs"
-                    onClick={() => {
-                        setMessage("You are horrible")
-                    }
-                    }
+                   // onClick={() => {
+                   //     setMessage('You are horrible!')
+                   // }}
+                    onClick={handleClick}
+                    // onChange={toggleNoDogs}
                 />
             </p>
             <p>{message}</p>
