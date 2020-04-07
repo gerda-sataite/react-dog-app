8dc794d3d6bd72db36dca6c07fc1bd4b568b3489
gerda-sataite <gerda.sataite@gmail.com>
2020-04-07 11:39:36 +0100

    Added a question

--- src/components/NoDogs.js
+++ src/components/NoDogs.js
@@ -6,7 +6,7 @@ function NoDogs() {
 
     const handleClick = () => {
         if (message === '') {
-            setMessage('You are horrible!')
+            setMessage('You are horrible! Would you like to see a cat?')
         } else {
             setMessage('')
         }
