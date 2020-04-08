9531fdc77a01ec7d5841e321f2013468ac5c9091
gerda-sataite <gerda.sataite@gmail.com>
2020-04-08 12:21:29 +0100

    Added a nav with Reach Router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import { Grommet } from 'grommet';
+import { Grommet, } from 'grommet';
+import { Router, Link } from "@reach/router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NoDogs from './components/NoDogs';
 import Dogs from './components/Dogs';
 import Quotes from './components/Quotes';
-import './App.css';
 import SearchDogs from './components/SearchDogs';
+import './App.css';
+
 
 function App() {
-
+  let Home = () => <div></div>
+  const NotFound = () => <p>Sorry, nothing here</p>
 
   return (
-    <column>
-      <Grommet plain>
-        <div className="App">
-          <h1>Random Dog Photos! </h1>
-          <Quotes />
 
-          <Dogs />
+    <Grommet plain>
 
-          <NoDogs />
+      <div>
+        <h1>Random Dog Photos! </h1>
+        
+        <nav>
+          <p><Link to="/">Home</Link>{" "}</p>
+          <p><Link to="/inspiration">Inspiration</Link></p>
+          <p><Link to="/dogs">Dogs</Link></p>
+          <p><Link to="/search-dogs">Search Breeds</Link></p>
+          <p><Link to="/no-dogs">I don't like dogs</Link></p>
+        </nav>
 
-          <br />
-          <SearchDogs />
-        </div>
-      </Grommet>
+
+        <Router>
+          <Home path="/" />
+          <Quotes path="/inspiration" />
+          <Dogs path="/dogs" />
+          <NoDogs path="/no-dogs" />
+          <SearchDogs path="/search-dogs" />
+          <NotFound default />
+        </Router>
+        
+      </div>
+    </Grommet>
 
 
-    </column>
+
   );
 }
 
