0cbbe4627185b7d875d4a2c2e9f9d61f4c171a47
gerda-sataite <gerda.sataite@gmail.com>
2020-04-09 12:22:58 +0100

    one image shows now if searched by breed, added the images in a box so they fit in column and if search button is clicked again after the image is shown, it will remove the image

--- src/components/SearchDogs.js
+++ src/components/SearchDogs.js
@@ -1,46 +1,76 @@
 import React from 'react';
 import axios from 'axios';
-import { Button } from 'grommet';
+import { Button, Box, Image } from 'grommet';
 
 function SearchDogs() {
     const [typedBreed, setTypedBreed] = React.useState("")
+    const [photoUrls, setPhotoUrls] = React.useState([])
 
     const getBreed = () => {
         const request = axios.get(`https://dog.ceo/api/breed/${typedBreed}/images`)
 
         request.then(response => {
-            console.log(response.data.message)
-
+            setPhotoUrls(response.data.message)
         })
     }
-
-    React.useEffect(() => {
-        getBreed();
-    });
+    // React.useEffect(() => {
+    //     getBreed();
+    // }, []);
     console.log(typedBreed)
 
-    return (
-        <div>
-            <p><input
-                value={typedBreed}
-                onChange={(event) => setTypedBreed(event.target.value)}
-            ></input>
-            </p>
-            <p><Button
-                label="Search"
-                onClick={() => {
-                    getBreed()
-                    setTypedBreed('')
-                }}
-            />
-            </p>
-            <p>
-                <image
-                    alt="Doggo"
-                    src={typedBreed.message}></image>
-            </p>
-        </div>
-    )
+    console.log('photoUrls:', photoUrls)
+
+    if (!photoUrls.length) {
+        return (
+            <div>
+                <p><input
+                    value={typedBreed}
+                    onChange={(event) => setTypedBreed(event.target.value)}
+                ></input>
+                </p>
+                <p><Button
+                    label="Search"
+                    onClick={() => {
+                        getBreed()
+                        setTypedBreed('')
+                    }}
+                />
+                </p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p><input
+                    value={typedBreed}
+                    onChange={(event) => setTypedBreed(event.target.value)}
+                ></input>
+                </p>
+                <p><Button
+                    label="Search"
+                    onClick={() => {
+                        getBreed()
+                        setTypedBreed('')
+                        setPhotoUrls([])
+                    }}
+                />
+                </p>
+                
+                <Box height="medium" width="large" alignSelf="center">
+                <Image
+                    alignSelf="center"
+                    fit="contain"
+                    src={photoUrls[0]}
+                />
+            </Box>
+                
+            </div>
+        )
+    }
+
+
+
+
 }
 
 export default SearchDogs
